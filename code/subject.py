5cdada000047a89eb57a99b1e89154a23587f363
BrenoCPimenta <brenocpimenta@gmail.com>
2021-02-07 18:01:09 -0300

    Add crossover with genetic diversity file

--- code/subject.py
+++ code/subject.py
@@ -27,13 +27,30 @@ class SubjectTree():
     
     def evalFunction(self, variable_values):
         return self.root.evalRecursion(variable_values)
-        
+
+    def getDepth(self):
+        return self.root.getNodeDepth()
+
+    def mutate(self):
+        depth = self.root.getNodeDepth()
+        mutation_depth = np.random.randint(depth+1)
+        #mutation_subsequent_tree_depth = (depth - mutation_depth)
+        self.root.mutate(mutation_depth)
+
+    def getSlice(self, depth_change):
+        return self.root.getSubTree(depth_change)
+
+    def transplant(self, new_node, depth_change):
+        return self.root.setSubTree(new_node, depth_change)
 
 class Node(SubjectTree):
     def __init__(self, num_layer, operators, operators_prob):        
+        self.num_layer = num_layer
+        self.operators = operators
+        self.operators_prob = operators_prob
 
         #Leaf
-        if num_layer <= 0:
+        if self.num_layer <= 0:
             self.node_type = 'terminal'
             self.terminal_type = np.random.choice(
                 ['constant', 'variable'], 
@@ -61,6 +78,96 @@ class Node(SubjectTree):
             self.variable_number = None
             self.left_node = Node(num_layer - 1, operators, operators_prob)
             self.right_node = Node(num_layer - 1, operators, operators_prob)
+
+    def getNodeDepth(self):
+        if self.node_type == 'terminal':
+            return 1
+        else:
+            return self.left_node.getNodeDepth() + 1
+    
+    def getSubTree(self, depth_change):
+        result = {"search_status" : "still_on_search", "root" : None}
+
+        if depth_change == 0 and self.node_type != 'terminal':
+            #if self.node_type == 'terminal':
+            result['search_status'] = "write_last_node"
+            return result
+        elif depth_change == 0 and self.node_type == 'terminal':
+            result['search_status'] = "last_terminal"
+            return result
+
+        #else:
+        side_decision = np.random.choice(['left', 'right'])
+        if side_decision == 'left':
+            if self.left_node is None:
+                return {"search_status" : "last_terminal", "root" : None}
+            else:
+                recursion_result = self.left_node.getSubTree(depth_change - 1)
+
+        elif side_decision == 'right':
+            if self.right_node is None:
+                return {"search_status" : "last_terminal", "root" : None}
+            else:
+                recursion_result = self.right_node.getSubTree(depth_change - 1)
+        else:
+            raise "getSubTree side decision problem"
+
+        if recursion_result['search_status'] == 'last_terminal':
+            recursion_result['search_status'] == 'write_last_node'
+            return recursion_result
+        elif recursion_result['search_status'] == 'write_last_node':
+            if side_decision == 'left':
+                new_root = copy.deepcopy(self.left_node)
+                return {"search_status" : "Found", "root" : new_root}
+            else:
+                new_root = copy.deepcopy(self.right_node)
+                return {"search_status" : "Found", "root" : new_root}
+        elif recursion_result['search_status'] == 'Found':
+            return recursion_result
+        else:
+            raise "getSubTree search_status problem"
+        
+
+    def setSubTree(self, new_node, depth_change):
+        side_decision = np.random.choice(['left', 'right'])
+        
+        if depth_change == 0:
+            if side_decision == 'left':
+                self.left_node = new_node
+            else:
+                self.right_node = new_node
+        else:
+            if side_decision == 'left':
+                self.left_node.setSubTree(new_node, depth_change-1)
+            else:
+                self.right_node.setSubTree(new_node, depth_change-1)
+
+
+    def mutate(self, depth):
+        if depth != 0: #keep digging
+            side_decision = np.random.choice(['left', 'right'])
+            if side_decision == 'left':
+                self.left_node.mutate(depth - 1)
+            elif side_decision == 'right':
+                self.right_node.mutate(depth - 1)
+            else:
+                raise "mutate point side decision problem"
+        else: #Mutate
+            if self.node_type == 'function':
+                self.node_funciton = np.random.choice(self.operators, p=self.operators_prob)
+            elif self.node_type == 'terminal':
+                self.terminal_type = np.random.choice(
+                    ['constant', 'variable'], 
+                    p=SubjectTree.constant_prob)
+                 #set terminal value
+                if self.terminal_type == 'variable':
+                    #only sets the position
+                    self.variable_number = SubjectTree.variable_counter
+                    SubjectTree.variable_counter += 1
+                else:
+                    self.variable_number = self._calculateConstant()
+            else:
+                raise "mutate point type problem"
 
     def printRecursion(self, variable_values, position=None):
         #Call inner left nodes:
